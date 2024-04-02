d0be42864be0a54b20202e766e48724615419a90
iH4nsen <isadora.hansen10@gmail.com>
2024-04-02 15:23:48 -0300

    .

--- App.js
+++ App.js
@@ -1,9 +1,12 @@
 import {View, Text, StyleSheet } from 'react-native';
 
+import Login from 'src/app/Login'
+import CreateAcount from 'src/app/CreateAcount'
+
 export default function App() {
   return (
     <View style={styles.container}>
-      
+      <Text>HOME</Text>
     </View>
   );
 }
