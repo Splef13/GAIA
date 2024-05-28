b9259548d6f0c9e12025094ad829f59807c79adf
David Junior <davidmundimjr@hotmail.com>
2024-05-28 16:58:05 -0300

    addPlant funcionando

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -1,29 +1,37 @@
+// FirebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore';
 import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { getDatabase, ref, set } from 'firebase/database';
+
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
-// Your web app's Firebase configuration
+//Firebase de testes
+/*
 const firebaseConfig = {
-    apiKey: "AIzaSyBOoMVIeUvOm1oKk53lJKCOAgcPCubl16w",
-    authDomain: "gaia-a59ea.firebaseapp.com",
-    databaseURL: "https://gaia-a59ea-default-rtdb.firebaseio.com",
-    projectId: "gaia-a59ea",
-    storageBucket: "gaia-a59ea.appspot.com",
-    messagingSenderId: "290936162422",
-    appId: "1:290936162422:web:9664c4a6cb2409a605b757"
-  };
+  apiKey: "AIzaSyD5is_Mv6CEdpEl-em52Ejf2AvvNwsUnBU",
+  authDomain: "gaia-testedavid.firebaseapp.com",
+  projectId: "gaia-testedavid",
+  storageBucket: "gaia-testedavid.appspot.com",
+  messagingSenderId: "740993891610",
+  appId: "1:740993891610:web:0946d344da7103016380a3"
+};
+*/
+//Firebase principal
+const firebaseConfig = {
+  apiKey: "AIzaSyBOoMVIeUvOm1oKk53lJKCOAgcPCubl16w",
+  authDomain: "gaia-a59ea.firebaseapp.com",
+  databaseURL: "https://gaia-a59ea-default-rtdb.firebaseio.com",
+  projectId: "gaia-a59ea",
+  storageBucket: "gaia-a59ea.appspot.com",
+  messagingSenderId: "290936162422",
+  appId: "1:290936162422:web:9664c4a6cb2409a605b757"
+};
 
-// Initialize Firebase App
-export const FIREBASE_APP = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication
-const auth = initializeAuth(FIREBASE_APP, {
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
 
-// Initialize Firebase Firestore
-export const FIREBASE_DB = getFirestore(FIREBASE_APP);
-
-// Export Firebase Authentication
-export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
+export { app as FIREBASE_APP, db as FIREBASE_DB, auth as FIREBASE_AUTH };
